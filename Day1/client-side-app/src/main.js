4c7ed3705ead3d57243a7745cf64993a6f917681
manishvbn <manishvbn@gmail.com>
2022-06-04 10:56:08 +0530

    1_datatypes folder added

--- Day1/client-side-app/src/main.js
+++ Day1/client-side-app/src/main.js
@@ -1,1 +1,6 @@
-console.log("Hello from Main File....");
+// console.log("Hello from Main File....");
+
+// Import an entire module, for side effects only, without importing anything from the file.
+// This will run the module's global code, but doesn't import any values.
+
+import './1_datatypes/1_declarations';
