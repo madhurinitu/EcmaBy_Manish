daca9668c9b4c3bcda70694ed4096667be628718
manishvbn <manishvbn@gmail.com>
2022-06-05 09:05:48 +0530

    Day2 Initial

---
+++ Day2/client-side-app/src/main.js
@@ -0,0 +1,4 @@
+// Import an entire module, for side effects only, without importing anything from the file.
+// This will run the module's global code, but doesn't import any values.
+
+import './1_functions/1_closures';
